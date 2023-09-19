97a841146e5320937fdeb025f9c019a5add64466
veermadav <115091583+veermadav@users.noreply.github.com>
2023-09-19 19:06:01 +0530

    Initial commit

---
+++ src/Components/Heading.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Heading() {
+  return <h1>my favourite foods </h1>;
+}
+export default Heading;
---
+++ src/Components/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function List() {
+  return (
+    <ul>
+      <li>pizza</li>
+      <li>burger</li>
+      <li>biriyani</li>
+    </ul>
+  );
+}
+export default List;
